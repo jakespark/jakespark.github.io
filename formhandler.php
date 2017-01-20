b4db381407aff8d803cc103d29fc08e1f2272fee
Jason Brown <jbrown947@charter.net>
2017-01-19 19:26:14 -0600

    added form

---
+++ formhandler.php
@@ -0,0 +1,21 @@
+ <?php 
+$to = 'jbrown947@charter.net';
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$subject = '$name Would Like More Info About 729 Studio!';
+$message = <<< EMAIL
+
+Hi! My name is $name.
+My email address is $email.
+$message
+
+EMAIL;
+$header = '$email';
+
+if($_POST) {
+	mail($to, $subject, $message, $header);
+	echo('Thank you. We usually respond within 24 hours.');
+} 
+?>
+  
